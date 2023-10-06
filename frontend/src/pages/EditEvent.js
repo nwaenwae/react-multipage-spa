7daa33c45c505ba446699f88d8a5451764369221
NwaeNandarAung97 <nandar.a@codetism.com>
2023-10-06 15:38:10 +0700

    use "react-router-dom" features

---
+++ frontend/src/pages/EditEvent.js
@@ -0,0 +1,8 @@
+import { useRouteLoaderData } from "react-router-dom";
+import EventForm from "../components/EventForm";
+function EditEventPage() {
+  const { event } = useRouteLoaderData("event-detail");
+  return <EventForm event={event} method="patch" />;
+}
+
+export default EditEventPage;
